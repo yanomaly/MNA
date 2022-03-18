a23f47c3d6459afffc76eb7a9d388f1033edd698
yanomaly <family.tolstoy@gmail.com>
2022-03-18 22:45:14 +0300

    Lab2 Gauss-Seidel not finished

--- src/Lab2/Lab2.java
+++ src/Lab2/Lab2.java
@@ -4,8 +4,8 @@ public class Lab2 {
 
     private static Double eps = Math.pow(0.1, 7);
 
-    public static void Newton(){
-        Double[] Xk = {-Math.PI/4, -2.3};
+    public static void Newton() {
+        Double[] Xk = {-Math.PI / 4, -2.3};
         Double[] Xk1 = {0.0, 0.0};
         Double[][] delta;
         Double norm = 1.0;
@@ -15,11 +15,11 @@ public class Lab2 {
         System.out.printf("%-27s", " x2(k): " + Xk[1]);
         System.out.printf("%-25s", " |x(k) - x(k+1)|: ");
         System.out.println();
-        while(norm > eps){
+        while (norm > eps) {
             iteration++;
             double cos = Math.cos(Xk[0] - 0.5 * Xk[1]);
             Double[][] F = {{cos + 1, -0.5 * cos + 2 * Xk[1]}, {32 * Xk[0], -2 * Xk[1]}};
-            Double[][] f = {{-(Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)}, { -(16 * Xk[0] * Xk[0] - Xk[1] * Xk[1] - 4)}};
+            Double[][] f = {{-(Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)}, {-(16 * Xk[0] * Xk[0] - Xk[1] * Xk[1] - 4)}};
             delta = Gauss_solution(F, f);
             Xk1[0] = Xk[0] + delta[0][0];
             Xk1[1] = Xk[1] + delta[1][0];
@@ -33,8 +33,9 @@ public class Lab2 {
         }
         System.out.println("Newton result: (" + Xk1[0] + ", " + Xk1[1] + ")");
     }
-    public static void Secants(){
-        Double[] Xk = {-Math.PI/4, -2.3};
+
+    public static void Secants() {
+        Double[] Xk = {-Math.PI / 4, -2.3};
         Double[] Xk1 = {0.0, 0.0};
         Double[][] delta;
         Double norm = 1.0;
@@ -47,7 +48,7 @@ public class Lab2 {
         iteration++;
         double cos = Math.cos(Xk[0] - 0.5 * Xk[1]);
         Double[][] F = {{cos + 1, -0.5 * cos + 2 * Xk[1]}, {32 * Xk[0], -2 * Xk[1]}};
-        Double[][] f = {{-(Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)}, { -(16 * Xk[0] * Xk[0] - Xk[1] * Xk[1] - 4)}};
+        Double[][] f = {{-(Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)}, {-(16 * Xk[0] * Xk[0] - Xk[1] * Xk[1] - 4)}};
         delta = Gauss_solution(F, f);
         Xk1[0] = Xk[0] + delta[0][0];
         Xk1[1] = Xk[1] + delta[1][0];
@@ -61,7 +62,7 @@ public class Lab2 {
         F[0][1] = ((Math.sin(Xk1[0] - 0.5 * Xk1[1]) + Xk1[0] + Xk1[1] * Xk1[1] - 5) - (Math.sin(Xk1[0] - 0.5 * Xk[1]) + Xk1[0] + Xk[1] * Xk[1] - 5)) / (Xk1[1] - Xk[1]);
         F[1][0] = ((16 * Xk1[0] * Xk1[0] - Xk1[1] * Xk1[1] - 4) - (16 * Xk[0] * Xk[0] - Xk1[1] * Xk1[1] - 4)) / (Xk1[0] - Xk[0]);
         F[1][1] = ((16 * Xk1[0] * Xk1[0] - Xk1[1] * Xk1[1] - 4) - (16 * Xk1[0] * Xk1[0] - Xk[1] * Xk[1] - 4)) / (Xk1[1] - Xk[1]);
-        while(norm > eps && iteration < 20){
+        while (norm > eps && iteration < 20) {
             iteration++;
             f = new Double[][]{{-(Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)}, {-(16 * Xk[0] * Xk[0] - Xk[1] * Xk[1] - 4)}};
             delta = Gauss_solution(F, f);
@@ -82,7 +83,43 @@ public class Lab2 {
         System.out.println("Newton result: (" + Xk1[0] + ", " + Xk1[1] + ")");
     }
 
-    public static Double[][] Gauss_solution(Double matrix_A[][], Double matrix_f[][]){
+    public static void Gauss_Seidel() {
+        Double[] Xk = {-Math.PI / 4, -2.3};
+        Double[] Xk1 = {0.0, 0.0};
+        double dlt = 1;
+        Double norm = 1.0;
+        int iteration = 0;
+        System.out.printf("%-15s", "iteration: " + iteration);
+        System.out.printf("%-27s", " x1(k): " + Xk[0]);
+        System.out.printf("%-27s", " x2(k): " + Xk[1]);
+        System.out.printf("%-25s", " |x(k) - x(k+1)|: ");
+        System.out.println();
+        while (norm > eps && iteration < 100) {
+            iteration++;
+            Double[] Xkc = Xk.clone();
+            while(dlt > eps){
+                Xk1[0] = Xk[0] - (Math.sin(Xk[0] - 0.5 * Xk[1]) + Xk[0] + Xk[1] * Xk[1] - 5)/(Math.cos(Xk[0] - 0.5 * Xk[1]) + 1);
+                dlt = Math.abs(Xk1[0] - Xk[0]);
+                Xk[0] = Xk1[0];
+            }
+            dlt = 1;
+            while(dlt > eps){
+                Xk1[1] = Xk[1] - (16 * Xk1[0] * Xk1[0] - Xk[1] * Xk[1] - 4)/(- 2 * Xk[1]);
+                dlt = Math.abs(Xk1[1] - Xk[1]);
+                Xk[1] = Xk1[1];
+            }
+            dlt = 1;
+            norm = Math.max(Math.abs(Xkc[0] - Xk1[0]), Math.abs(Xkc[1] - Xk1[1]));
+            System.out.printf("%-15s", "iteration: " + iteration);
+            System.out.printf("%-27s", " x1(k): " + Xk1[0]);
+            System.out.printf("%-27s", " x2(k): " + Xk1[1]);
+            System.out.printf("%-25s", " |x(k) - x(k+1)|: " + norm);
+            System.out.println();
+        }
+        System.out.println("Gauss-Seidel result: (" + Xk1[0] + ", " + Xk1[1] + ")");
+    }
+
+    public static Double[][] Gauss_solution(Double matrix_A[][], Double matrix_f[][]) {
         Double solution[][] = new Double[matrix_A.length][1];
         Double max_abs;
         Double temp[];
@@ -94,13 +131,13 @@ public class Lab2 {
             max_abs = Math.abs(matrix_A[i][i]);
             max_abs_j = i;
             for (int j = i; j < matrix_A.length; j++) {
-                if(Math.abs(max_abs) < Math.abs(matrix_A[j][i])) {
+                if (Math.abs(max_abs) < Math.abs(matrix_A[j][i])) {
                     max_abs = matrix_A[j][i];
                     max_abs_j = j;
                 }
             }
 //Перестановка строки с максимальным элементом
-            if(max_abs_j != i) {
+            if (max_abs_j != i) {
                 temp = matrix_A[i];
                 matrix_A[i] = matrix_A[max_abs_j];
                 matrix_A[max_abs_j] = temp;
@@ -114,7 +151,7 @@ public class Lab2 {
                 matrix_A[i][j] /= a_i_i;
             matrix_f[i][0] /= a_i_i;
 //Зануление столбца
-            if(i < matrix_A.length - 1) {
+            if (i < matrix_A.length - 1) {
                 for (int j = i; j < matrix_A.length - 1; j++) {
                     a_i_i1 = matrix_A[j + 1][i];
                     for (int k = 0; k < matrix_A.length; k++) {
@@ -127,7 +164,7 @@ public class Lab2 {
 //Обратный ход
         int count = 0;
         double temp_solution = 0;
-        for (int i = matrix_A.length - 1; i > -1 ; i--) {
+        for (int i = matrix_A.length - 1; i > -1; i--) {
             temp_solution = matrix_f[i][0];
             for (int j = 0, k = matrix_A.length - 1; j < count && k > 0; j++, k--)
                 temp_solution -= matrix_A[i][k] * solution[k][0];
@@ -141,5 +178,12 @@ public class Lab2 {
         Lab2.Newton();
         System.out.println();
         Lab2.Secants();
+        System.out.println();
+        Lab2.Gauss_Seidel();
     }
 }
+
+
+
+
+
